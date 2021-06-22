2f2656e17375a5cfea24ddeac328d3ca444101fd
Donik1984 <86175071+Donik1984@users.noreply.github.com>
2021-06-22 12:07:40 -0400

    Starting Communication Log

---
+++ CommunicationLog/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CommunicationLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Pilot");
+        }
+    }
+}
