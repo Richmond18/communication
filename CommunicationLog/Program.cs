e4886a5bd848c1b8dbc9b7de1c5873dac915349a
rashida richmond <richmond1895@gmail.com>
2021-07-27 14:30:20 -0400

    comms

--- CommunicationLog/Program.cs
+++ CommunicationLog/Program.cs
@@ -11,6 +11,8 @@ namespace CommunicationLog
 =======
             Console.WriteLine("Hello Hello Pilot Rashida");
             Console.WriteLine("What is going on");
+            Console.WriteLine("I'm doing well, how about you?");
+
 >>>>>>> 72079f962452b2b47b62c9454e91bd75c1244f28
         }
     }
